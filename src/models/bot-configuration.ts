361fc78ff760748f6d9d5b5c00e92878943a8cfb
groc-prog <marc.troisner@gmail.com>
2025-10-02 18:48:48 +0200

    feat(model): add database models

---
+++ src/models/bot-configuration.ts
@@ -0,0 +1,41 @@
+import * as mongoose from 'mongoose';
+
+const botConfigurationSchema = new mongoose.Schema(
+  {
+    /**
+     * The unique ID of the discord channel the bot will use to broadcast his messages.
+     */
+    broadcastChannelId: {
+      type: mongoose.SchemaTypes.String,
+      unique: true,
+    },
+    guildId: {
+      type: mongoose.SchemaTypes.String,
+      index: true,
+      unique: true,
+      required: true,
+    },
+    /**
+     * A custom schedule for when the bot will broadcast his messages.
+     */
+    broadcastCronSchedule: {
+      type: mongoose.SchemaTypes.String,
+      trim: true,
+    },
+    /**
+     * The unique ID of the discord user who modified the configuration last. Will initialize
+     * with the unique ID of the bot.
+     */
+    lastModifiedBy: {
+      type: mongoose.SchemaTypes.String,
+      trim: true,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export type BotConfiguration = mongoose.InferSchemaType<typeof botConfigurationSchema>;
+export const BotConfigurationModel = mongoose.model('BotConfiguration', botConfigurationSchema);
---
+++ src/models/user-notification.ts
@@ -0,0 +1,85 @@
+import { Locale } from 'discord.js';
+import * as mongoose from 'mongoose';
+
+export enum KeywordType {
+  MovieTitle = 'title',
+  MovieFeature = 'feature',
+}
+
+const keywordSchema = new mongoose.Schema({
+  /**
+   * The type of keyword, from which the resulting fuzzy search is inferred.
+   */
+  type: {
+    type: mongoose.SchemaTypes.String,
+    required: true,
+    enum: Object.values(KeywordType),
+  },
+  /**
+   * The value to try to match when fuzzy searching existing records.
+   */
+  value: {
+    type: mongoose.SchemaTypes.String,
+    required: true,
+  },
+});
+
+const notificationSchema = new mongoose.Schema(
+  {
+    keywords: [keywordSchema],
+    /**
+     * The number of notifications already sent to the user. Used to check when the bot should
+     * stop notifying the user about movies matching the entries keywords. Incremented each
+     * time a notification is send. Will be `undefined` if `maxNotifications` is not set.
+     */
+    sentNotifications: mongoose.SchemaTypes.Number,
+    /**
+     * The maximum number of notifications the user should receive for this notification. After the
+     * maximum number of notifications is reached, the notification is removed.
+     */
+    maxNotifications: mongoose.SchemaTypes.Number,
+    /**
+     * UTC timestamp of when the last notification was sent.
+     */
+    lastNotificationSentAt: mongoose.SchemaTypes.Date,
+    /**
+     * Date (12:00AM UTC) of when the notification should expire. Once expired, the notification
+     * will be removed.
+     */
+    expiresAt: mongoose.SchemaTypes.Date,
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const userNotificationSchema = new mongoose.Schema(
+  {
+    /**
+     * The unique ID of the user who will receive notifications.
+     */
+    userId: {
+      type: mongoose.SchemaTypes.String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    /**
+     * The locale in which the notification will be. This value is not inferred by the users settings
+     * (as this is not exposed in the discord API) but rather from the guild in which he creates the
+     * notification.
+     */
+    locale: {
+      type: mongoose.SchemaTypes.String,
+      required: true,
+      enum: Object.values(Locale),
+    },
+    notifications: [notificationSchema],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export type UserNotification = mongoose.InferSchemaType<typeof userNotificationSchema>;
+export const UserNotificationModel = mongoose.model('UserNotification', userNotificationSchema);
