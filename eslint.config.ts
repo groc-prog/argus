8c1408b1a6cf34d14fd7a2893108a69c160041d3
groc-prog <marc.troisner@gmail.com>
2025-10-02 17:10:30 +0200

    chore: project setup

---
+++ eslint.config.ts
@@ -0,0 +1,24 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: globals.node,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: eslintConfigPrettier.rules,
+  },
+  tseslint.configs.strictTypeChecked,
+]);
