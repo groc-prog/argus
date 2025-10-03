4242eeb75697b42a9d130ee726ba22bbbb7d644d
groc-prog <marc.troisner@gmail.com>
2025-10-03 12:06:30 +0200

    feat(bot): add guild delete event for configuration cleanup

---
+++ src/bot/events/guild-delete.ts
@@ -0,0 +1,22 @@
+import { Events, Guild } from 'discord.js';
+import logger from '../../utilities/logger';
+import { BotConfigurationModel } from '../../models/bot-configuration';
+
+export default {
+  name: Events.GuildDelete,
+
+  async execute(guild: Guild) {
+    const loggerWithCtx = logger.child({ guildId: guild.id });
+    loggerWithCtx.info('Bot removed from guild, cleaning up bot configuration');
+
+    try {
+      loggerWithCtx.debug('Checking if guild has existing bot configuration');
+      const existingBotConfiguration = await BotConfigurationModel.deleteOne({ guildId: guild.id });
+      if (existingBotConfiguration.deletedCount === 0)
+        loggerWithCtx.info('No existing bot configuration found, skipping');
+      else loggerWithCtx.info('Bot configuration cleanup finished');
+    } catch (err) {
+      loggerWithCtx.error({ err }, 'Failed to clean up bot configuration');
+    }
+  },
+};
