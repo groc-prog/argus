6ee6c3eaf936a9af3f0171ad777281504dfd46d3
groc-prog <marc.troisner@gmail.com>
2025-10-24 18:54:51 +0200

    ci: add workflows

--- release.config.mjs
+++ release.config.mjs
@@ -7,7 +7,7 @@ export default {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
-    '@semantic-release/npm',
+    ['@semantic-release/npm', { npmPublish: false }],
     [
       '@semantic-release/git',
       {
